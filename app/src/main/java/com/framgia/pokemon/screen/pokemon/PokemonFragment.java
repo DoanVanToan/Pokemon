fbd4761a2d5fae3b4fcac16d60aa2b1f9388fc86
ducpm3009 <hklbndvl@gmail.com>
2017-08-23 19:55:03 +0700

    pokemon_fragment_UI

---
+++ app/src/main/java/com/framgia/pokemon/screen/pokemon/PokemonFragment.java
@@ -0,0 +1,37 @@
+package com.framgia.pokemon.screen.pokemon;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.framgia.pokemon.R;
+
+/**
+ * Pokemon Screen.
+ */
+public class PokemonFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+            @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_pokemon, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+}
